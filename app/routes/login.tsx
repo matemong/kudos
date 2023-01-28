ddecf43766e7098e75e2efc5a243213d8b4764ac
Mate Mong <mongmate811@gmail.com>
2023-01-28 22:56:35 +0100

    login form functions

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,6 +1,22 @@
 import { Layout } from "~/components/layout";
-
+import { FormField } from "~/components/form-field";
+import { useState } from "react";
 export default function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    field: string
+  ) => {
+    setFormData((form) => ({
+      ...form,
+      [field]: event.target.value,
+    }));
+  };
+
   return (
     <Layout>
       <div className="h-full flex justify-center items-center flex-col gap-y-4">
@@ -12,23 +28,18 @@ export default function Login() {
         </p>
 
         <form className="rounded-2xl bg-gray-200 p-6 w-96">
-          <label htmlFor="email" className="text-blue-600 font-semibold">
-            Email
-          </label>
-          <input
-            type="text"
-            id="email"
-            name="email"
-            className="w-full p-2 rounded-xl my-2"
+          <FormField
+            htmlFor="email"
+            label="Email"
+            value={formData.email}
+            onChange={e => handleInputChange(e, 'email')}
           />
-          <label htmlFor="password" className="text-blue-600 font-semibold">
-            Password
-          </label>
-          <input
+          <FormField
+            htmlFor="password"
+            label="password"
             type="password"
-            id="password"
-            name="password"
-            className="w-full p-2 rounded-xl my-2"
+            value={formData.password}
+            onChange={e => handleInputChange(e, 'password')}
           />
 
           <div className="w-full text-center">
