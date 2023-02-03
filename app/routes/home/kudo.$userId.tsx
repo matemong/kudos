43cbaf1ccb8458f47281805dca620c13d442620a
Mate Mong <mongmate811@gmail.com>
2023-02-03 22:49:26 +0100

    fetch user by id when clicking on profile picture

--- app/routes/home/kudo.$userId.tsx
+++ app/routes/home/kudo.$userId.tsx
@@ -1,9 +1,17 @@
-import { json, LoaderFunction } from "@remix-run/node";
+import { json, LoaderFunction, redirect } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { getUserById } from "~/utils/users.server";
 
 export const loader: LoaderFunction = async ({ request, params }) => {
   const { userId } = params;
-  return json({ userId });
+
+  if (typeof userId !== "string") {
+    return redirect("/home");
+  }
+
+  const recipient = await getUserById(userId);
+
+  return json({ recipient });
 };
 
 export default function KudoModal() {
