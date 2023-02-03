d07fb09739c28adab559443c4d1f4c23cbc56883
Mate Mong <mongmate811@gmail.com>
2023-02-03 22:40:11 +0100

    nested kudo route

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,7 +3,7 @@ import { requireUserId } from "~/utils/auth.server";
 import { Layout } from "~/components/layout";
 import { UserPanel } from "~/components/user-panel";
 import { getOtherUsers } from "~/utils/users.server";
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData, Outlet } from "@remix-run/react";
 
 export const loader: LoaderFunction = async ({ request }) => {
   const userId = await requireUserId(request);
@@ -15,6 +15,7 @@ export default function Home() {
     const {users} = useLoaderData();
   return (
     <Layout>
+        <Outlet></Outlet>
       <div className="h-full flex">
         <UserPanel users={users} />
       </div>
