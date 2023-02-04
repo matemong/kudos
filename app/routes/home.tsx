b07ad9aff0c114aa7d8290dc9ddc2a0212f953b0
Mate Mong <mongmate811@gmail.com>
2023-02-04 16:19:30 +0100

    search bar with filter working

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,5 @@
-import { json, LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import type { LoaderFunction } from "@remix-run/node";
 import { requireUserId } from "~/utils/auth.server";
 import { Layout } from "~/components/layout";
 import { UserPanel } from "~/components/user-panel";
@@ -6,7 +7,9 @@ import { getOtherUsers } from "~/utils/users.server";
 import { useLoaderData, Outlet } from "@remix-run/react";
 import { getFilteredKudos } from "~/utils/kudos.server";
 import { Kudo } from "~/components/kudo";
-import { Kudo as IKudo, Profile } from "@prisma/client";
+import type { Kudo as IKudo, Profile } from "@prisma/client";
+import { Prisma } from "@prisma/client";
+import { SearchBar } from "~/components/search-bar";
 
 interface KudoWithProfile extends IKudo {
   author: {
@@ -17,8 +20,49 @@ interface KudoWithProfile extends IKudo {
 export const loader: LoaderFunction = async ({ request }) => {
   const userId = await requireUserId(request);
   const users = await getOtherUsers(userId);
-  const kudos = await getFilteredKudos(userId, {}, {});
+  const url = new URL(request.url);
+  const sort = url.searchParams.get("sort");
+  const filter = url.searchParams.get("filter");
+  let sortOptions: Prisma.KudoOrderByWithRelationInput = {};
 
+  if (sort) {
+    if (sort === "date") {
+      sortOptions = { createdAt: "desc" };
+    }
+    if (sort === "sender") {
+      sortOptions = { author: { profile: { firstName: "asc" } } };
+    }
+    if (sort === "emoji") {
+      sortOptions = { style: { emoji: "asc" } };
+    }
+  }
+  let textFilter: Prisma.KudoWhereInput = {};
+
+  if (filter) {
+    textFilter = {
+      OR: [
+        { message: { mode: "insensitive", contains: filter } },
+        {
+          author: {
+            OR: [
+              {
+                profile: {
+                  is: { firstName: { mode: "insensitive", contains: filter } },
+                },
+              },
+              {
+                profile: {
+                  is: { lastName: { mode: "insensitive", contains: filter } },
+                },
+              },
+            ],
+          },
+        },
+      ],
+    };
+  }
+
+  const kudos = await getFilteredKudos(userId, sortOptions, textFilter);
   return json({ users, kudos });
 };
 
@@ -33,6 +77,7 @@ export default function Home() {
         <UserPanel users={users} />
 
         <div className="flex-1 flex flex-col">
+          <SearchBar />
 
           <div className="flex-1 flex">
             <div className="w-full p-10 flex flex-col gap-y-4">
@@ -40,7 +85,6 @@ export default function Home() {
                 <Kudo key={kudo.id} kudo={kudo} profile={kudo.author.profile} />
               ))}
             </div>
-
           </div>
         </div>
       </div>
