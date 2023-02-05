322443f9ed83e3a4313e464e8e34c46b7fa1deac
Mate Mong <mongmate811@gmail.com>
2023-02-05 22:43:14 +0100

    profile form

--- app/routes/home/profile.tsx
+++ app/routes/home/profile.tsx
@@ -2,7 +2,11 @@ import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { Modal } from "~/components/modal";
+import { useState } from "react";
+import { FormField } from "~/components/form-field";
 import { getUser } from "~/utils/auth.server";
+import { departments } from "~/utils/constants";
+import { SelectBox } from "~/components/select-box";
 
 export const loader: LoaderFunction = async ({ request }) => {
   const user = await getUser(request);
@@ -12,6 +16,18 @@ export const loader: LoaderFunction = async ({ request }) => {
 
 export default function ProfileSettings() {
   const { user } = useLoaderData();
+  const [formData, setFormData] = useState({
+    firstName: user?.profile?.firstName,
+    lastName: user?.profile?.lastName,
+    department: user?.profile?.department || "MARKETING",
+  });
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    field: string
+  ) => {
+    setFormData((form) => ({ ...form, [field]: event.target.value }));
+  };
 
   return (
     <Modal isOpen={true} className="w-1/3">
@@ -19,6 +35,40 @@ export default function ProfileSettings() {
         <h2 className="text-4xl font-semibold text-blue-600 text-center mb-4">
           Your Profile
         </h2>
+        <div className="flex">
+          <div className="flex-1">
+            <form method="post">
+              <FormField
+                htmlFor="firstName"
+                label="First Name"
+                value={formData.firstName}
+                onChange={(e) => handleInputChange(e, "firstName")}
+              />
+
+              <FormField
+                htmlFor="lastName"
+                label="Last Name"
+                value={formData.lastName}
+                onChange={(e) => handleInputChange(e, "lastName")}
+              />
+              <SelectBox
+                className="w-full rounded-xl px-3 py-2 text-gray-400"
+                id="department"
+                label="Department"
+                name="department"
+                options={departments}
+                value={formData.department}
+                onChange={(e) => handleInputChange(e, "department")}
+              />
+
+              <div className="w-full text-right mt-4">
+                <button className="rounded-xl bg-yellow-300 font-semibold text-blue-600 px-16 py-2 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1">
+                  Save
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </Modal>
   );
