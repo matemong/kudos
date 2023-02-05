5c769d884829fbded989788108fa1e27aff6df21
Mate Mong <mongmate811@gmail.com>
2023-02-05 22:29:45 +0100

    profile modal

---
+++ app/routes/home/profile.tsx
@@ -0,0 +1,25 @@
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { Modal } from "~/components/modal";
+import { getUser } from "~/utils/auth.server";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const user = await getUser(request);
+
+  return json({ user });
+};
+
+export default function ProfileSettings() {
+  const { user } = useLoaderData();
+
+  return (
+    <Modal isOpen={true} className="w-1/3">
+      <div className="p-3">
+        <h2 className="text-4xl font-semibold text-blue-600 text-center mb-4">
+          Your Profile
+        </h2>
+      </div>
+    </Modal>
+  );
+}
